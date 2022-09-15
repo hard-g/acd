d48351fe25e2095ba1b9c5d56cbc000e6db7659a
Boone B Gorges <boonebgorges@gmail.com>
2022-09-15 14:37:30 -0500

    Remove external jquery-scrollto plugin.
    It was 404ing and it doesn't appear to be used anywhere in the app.

--- footer.php
+++ footer.php
@@ -17,9 +17,6 @@
 <!-- jQuery -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
-<!-- jQuery ScrollTo Plugin -->
-<script src="//balupton.github.io/jquery-scrollto/lib/jquery-scrollto.js"></script>
-
 <!-- History.js -->
 <script src="//browserstate.github.io/history.js/scripts/bundled/html4+html5/jquery.history.js"></script>
 
