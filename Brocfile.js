7b44d4d204fcb255bfcb3d9f306d0e50f228f574
Boone B Gorges <boonebgorges@gmail.com>
2022-09-15 12:06:17 -0500

    Migrate broccoli-sass to broccoli-sass-source-maps
    The old module is incompatible with modern NPM.

--- Brocfile.js
+++ Brocfile.js
@@ -1,12 +1,12 @@
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
-var compileSass = require('broccoli-sass');
-var autoprefixer = require('broccoli-autoprefixer'); 
+var compileSass = require('broccoli-sass-source-maps')(require('sass'))
+var autoprefixer = require('broccoli-autoprefixer');
 var es6transpiler = require('broccoli-es6-transpiler');
 var imagemin = require('broccoli-imagemin');
 var browserify = require('broccoli-fast-browserify');
 
-// Specify directories  
+// Specify directories
 var svgsDir = 'assets/svg';
 var sassDir = 'assets/sass';
 var scriptDir = 'assets/app';
@@ -39,11 +39,13 @@ var images = pickFiles('assets', {
 /*
 	CSS
 */
+
 var sass = compileSass([sassDir], 'style.scss', 'assets/style/style.css');
 var appCSS = autoprefixer(sass, {
   sourcemap: true,
   browsers: ['> 1%', 'last 2 versions', 'Chrome 5', 'Firefox 6']
 });
+
 
 /*
 	JS
