37edd01b2bb4f0487919f3ee040f4c8b81610471
Boone B Gorges <boonebgorges@gmail.com>
2022-09-19 09:35:43 -0500

    Port single-essay.php changes from production.

--- single-essay.php
+++ single-essay.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	get_header();
 
 	$html = '';
@@ -23,14 +23,41 @@
 
 			// print_r($postContent);
 			$html .= '<div class="page-title-wrapper"><h1 class="page-title">'. get_the_title() .'</h1></div>';
+		if($postContent['hero_caption']){
+			$html .= '<div class="essay-hero wrapper"><img class="essay-hero-image" src="'.$postContent['hero_image'].'"/><div class="image-caption-ctr">'. $postContent['hero_caption'] .'</div></div>';
+		}else{
 			$html .= '<div class="essay-hero wrapper"><img class="essay-hero-image" src="'.$postContent['hero_image'].'"/></div>';
+		}
 			// $html .= '<section class="wrapper essay-content">'.do_shortcode(get_the_content()).'</section>';
-			$html .= '<section class="wrapper essay-content">'.apply_filters('the_content', get_the_content()).'</section>';
+			$html .= '<section class="wrapper essay-content">'.apply_filters('the_content', get_the_content());
+			foreach($postContent['essay_index_section'] as $weirdessaysection) {
+				$html .='<hr style="width: 240px; margin: 0 auto;" />';
+				$html .='<p class="lower-essay-header"> '. $weirdessaysection['index_title'] . '</p><div class="creditss">
+<div class="column column__count">';
+				foreach($weirdessaysection['essay_index'] as $essayindexer ) {
+					$html .='<div class="index-entery"><p class="uni">' . $essayindexer['publication'] .'</p>';
+					$html .='<p class="link"><a href=" '. $essayindexer['title_link'] .'" target="_blank" rel="noopener">' . $essayindexer['title'] .'</a></p>';
+					$html .='<p class="index-body">'. $essayindexer['description'] .'</p>';
+					if ( $essayindexer['date'] == true ){
+					$html .='<div class="date-wrap">
+<p class="date-title">Date</p>
+<p class="date-int">'.  $essayindexer['date'].'</p>
+</div>';};
+					if ( $essayindexer['author'] == true ){
+					$html .='<div class="date-wrap">
+<p class="date-title">Authors</p>
+<p class="date-int">'. $essayindexer['author'] .'</p>
+</div>';};
+				$html .='</div>';
+				};
+				 $html .='</div></div>';
+			};
+			$html .='</section>';
 
 			$next_post = get_adjacent_post(null, null, false);
 			$prev_post = get_adjacent_post(null, null, true);
 
-			
+
 			// print_r($next_post);
 			// print_r($prev_post);
 
@@ -45,6 +72,8 @@
 				$html .= '<div class="inner"><img style="width: 100%; height: auto;" src="'.$prevPostContent['thumbnail_image'].'" />';
 				$html .= '<span class="item-title">'.get_the_title($prev_post->ID).'</span>';
 				$html .= '</div>';
+			} else {
+				$html .= '<a class="flexer no-cursor"></a>';
 			}
 			$html .= '</div>';
 			// end prev
@@ -57,10 +86,20 @@
 				$html .= '<div class="inner"><img style="width: 100%; height: auto;" src="'.$nextPostContent['thumbnail_image'].'" />';
 				$html .= '<span class="item-title">'.get_the_title($next_post->ID).'</span>';
 				$html .= '</div>';
+			} else {
+
+				$morePostContent = get_fields(1137);
+				$html .= '<a class="flexer" href="'.get_site_url().'/index.php/how-leaders-of-the-university-of-minnesota-used-and-abused-power/">';
+				$html .= '<span class="essay-header-inline">INTERACTIVE FEATURE</span>';
+				$html .= '<div class="trans-bg"><span class="description">'.$nextPostContent['excerpt'].'</span></div>';
+				$html .= '<div class="inner"><img style="width: 100%; height: auto;" src="'.$morePostContent['thumbnail_image'].'" />';
+				$html .= '<span class="item-title">A Deep Dive into Documents about Power</span>';
+				$html .= '</div>';
+			}
 			}
 			$html .= '</div>';
 			// if next
-			
+
 			// end next
 			$html .= '</section>';
 	  }
